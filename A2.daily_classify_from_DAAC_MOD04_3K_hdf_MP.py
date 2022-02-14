46ae7eba25eebb2ffee87186a3d6eb1ceef1412b
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-02-15 00:22:20 +0900

    make class

--- A2.daily_classify_from_DAAC_MOD04_3K_hdf_MP.py
+++ A2.daily_classify_from_DAAC_MOD04_3K_hdf_MP.py
@@ -12,41 +12,37 @@ import MODIS_AOD_utilities
 import Python_utilities
 from datetime import datetime
 
-#threading library
-import threading
-import sys
-
 #########################################
 from multiprocessing import Process, Queue
 
 class Multiprocessor():
-	def __init__(self):
-		self.processes = []
-		self.queue = Queue()
+    def __init__(self):
+        self.processes = []
+        self.queue = Queue()
 
-	@staticmethod
-	def _wrapper(func, queue, args, kwargs):
-		ret = func(*args, **kwargs)
-		queue.put(ret)
+    @staticmethod
+    def _wrapper(func, queue, args, kwargs):
+        ret = func(*args, **kwargs)
+        queue.put(ret)
 
-	def restart(self):
-		self.processes = []
-		self.queue = Queue()
+    def restart(self):
+        self.processes = []
+        self.queue = Queue()
 
-	def run(self, func, *args, **kwargs):
-		args2 = [func, self.queue, args, kwargs]
-		p = Process(target=self._wrapper, args=args2)
-		self.processes.append(p)
-		p.start()
+    def run(self, func, *args, **kwargs):
+        args2 = [func, self.queue, args, kwargs]
+        p = Process(target=self._wrapper, args=args2)
+        self.processes.append(p)
+        p.start()
 
-	def wait(self):
-		rets = []
-		for p in self.processes:
-			ret = self.queue.get()
-			rets.append(ret)
-		for p in self.processes:
-			p.join()
-		return rets
+    def wait(self):
+        rets = []
+        for p in self.processes:
+            ret = self.queue.get()
+            rets.append(ret)
+        for p in self.processes:
+            p.join()
+        return rets
 
 
 #########################################
